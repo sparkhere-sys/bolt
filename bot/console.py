87b2ac7edb6e15776c957a8ae328e68e6a3fad2a
_Spark <spark-aur@proton.me>
2025-08-16 09:31:32 +0300

    oh boy! i love python errors because of circular imports! (god im a fucking idiot)

--- bot/console.py
+++ bot/console.py
@@ -6,11 +6,17 @@ import time
 
 ## pypkg
 
-from bot.constants import *
+## from bot.constants import *
+from bot.constants import log_colors, reset_colors # even though we used to have a * import, 
+                                                   # we have to lazy import those two variables
+                                                   # for some godforsaken reason
 
 # FUNCTIONS
 
 def log(msg, level="LOG"):
+  # why the hell did i have to add a lazy import
+  # screw you python.
+
   print(f"{log_colors[level.upper()]}[{level.upper()}]{reset_colors} [{time.asctime(time.gmtime())}] {msg}")
   # in plain english,
   # it just outputs:
