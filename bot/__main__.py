22b255750f749036e68a5b4e41bedc7ab5a1dacc
_Spark <spark-aur@proton.me>
2025-08-16 10:13:05 +0300

    clean up __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,13 +1,24 @@
 # bot/__main__.py
 
+# LIBRARIES AND MODULES
+
+## pypkg
+
 import bot.bot as bot
 import bot.console as console
 
-if __name__ == "__main__":
+# FUNCTIONS
+
+def main():
   try:
     console.log("Starting Bolt...", "LOG")
-    bot.run()
+    bot.start_bot()
   except Exception as e:
     console.log(f"Something happened. exception: {e}", "FATAL")
   except KeyboardInterrupt:
     console.log(f"Bolt shutting down...", "LOG")
+
+# STARTUP
+
+if __name__ == "__main__":
+  main()
