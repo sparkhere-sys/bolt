d57ba00657e61e310ec935210a45abaebabda3a6
_Spark <spark-aur@proton.me>
2026-08-10 10:43:38 +0200

    logging to a file
    resolves #70

--- bot/console.py
+++ bot/console.py
@@ -2,8 +2,9 @@
 
 # IMPORTS
 
-import time # TODO: replace with datetime maybe
+import time
 from typing import Any
+from pathlib import Path
 from traceback import format_exc
 
 ## bolt
@@ -14,15 +15,27 @@ import bot.constants.toml as toml_config
 # FUNCTIONS
 
 def format_msg(msg: Any, level: LogLevel) -> None:
+  log_dir = Path("logs")
+  log_dir.mkdir(exist_ok=True)
+
+  log_file = log_dir / f"{time.strftime('%Y-%m-%d')}.log"
+  
   if level.name in toml_config.levels_to_ignore:
     return
 
   level_str = level.badge
   time_str = f"{dim}{time.asctime(time.localtime())}{reset}"
 
+  level_str_plain = level.plain_badge
+  time_str_plain = f"{time.asctime(time.localtime())}"
+
   full = f"{level_str} {time_str} {msg}"
+  full_plain = f"{level_str_plain} {time_str_plain} {msg}"
 
   print(full)
+
+  with log_file.open("a", encoding="utf-8") as file:
+    file.write(full_plain + "\n")
 
 def log(msg: Any) -> None:
   format_msg(msg, LogLevel.LOG)
